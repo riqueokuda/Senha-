6391f67e6bed7d0d045f35ec0a53538012575571
Lucas Renó <lucasreno9@gmail.com>
2022-10-18 19:08:14 -0300

    Projeto funcional

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  senha: string = "";
+  forca: string[] = ["...", "FRACA", "MÉDIA", "FORTE", "MUITO FORTE"];
+  nivel: number = 0;
+
+  constructor() {}
+
+  verificar(){
+    this.nivel = 0;
+    if(this.senha.length >= 8){
+      this.nivel++;
+    }
+    if(/[a-z]/.test(this.senha)){
+      this.nivel++;
+    }
+    if(/[A-Z]/.test(this.senha)){
+      this.nivel++;
+    }
+    if(/\d/.test(this.senha)){
+      this.nivel++;
+    }
+  }
+}
